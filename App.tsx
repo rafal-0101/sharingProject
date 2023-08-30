74d382ea828bd7ae4cb29c06a352b6303925ae6f
michalinapepel <misia.pepel@onet.pl>
2023-08-30 17:00:55 +0200

    Add BarCode Scanner

--- App.tsx
+++ App.tsx
@@ -1,12 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import Form from './components/Form';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import Form from "./components/Form";
+import BarcodeScanner from "./components/BarcodeScanner";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>HELOŁ</Text>
       <Form />
+      <BarcodeScanner />
       <StatusBar style="auto" />
     </View>
   );
@@ -15,8 +17,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
